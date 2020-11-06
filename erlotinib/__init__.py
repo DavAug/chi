8ed4ba03176ca19e4788359b8546026fd48df914
DavAug <david.augustin@gmx.net>
2020-11-06 16:23:04 +0000

    delete dummy test

--- erlotinib/__init__.py
+++ erlotinib/__init__.py
@@ -5,8 +5,4 @@
 # full license details.
 #
 
-from .dummy import (  # noqa
-    dummy_function
-)
-
 from . import plots  # noqa
--- erlotinib/dummy.py
+++
@@ -1,9 +0,0 @@
-#
-# This file is part of the erlotinib repository
-# (https://github.com/DavAug/erlotinib/) which is released under the
-# BSD 3-clause license. See accompanying LICENSE.md for copyright notice and
-# full license details.
-#
-
-def dummy_function():
-    return 42
