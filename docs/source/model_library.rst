700f75e246a13323857d44b0b0dcbd233b9e478a
DavAug <david.augustin@gmx.net>
2020-11-10 14:51:18 +0000

    improved documentation of the model library

--- docs/source/model_library.rst
+++ docs/source/model_library.rst
@@ -4,9 +4,14 @@ Model Library
 
 .. currentmodule:: erlotinib
 
-The data library contains a number of datasets where the
-pharmacokinetic and pharmacodynamic properties of erlotinib
-have been explored.
+The model library contains a number of pharmacokinetic and
+pharmacodynamic models in SBML file format which have been
+used to model the PKPD of erlotinib.
+
+Those SBML models can be passed to either a
+:class:`PharmacokineticModel` or a
+:class:`PharmacodynamicModel` for simulation or to learn the
+model parameters from data.
 
 
 .. autoclass:: ModelLibrary
--- erlotinib/_model_library_api.py
+++ erlotinib/_model_library_api.py
@@ -18,15 +18,40 @@ class ModelLibrary(object):
         self._path = os.path.dirname(os.path.abspath(__file__))
         self._path += '/model_library/'
 
-    def tumour_growth_inhibition_pd_model(self):
+    def tumour_growth_inhibition_model_koch(self):
         r"""
-        Returns the absolute path to a tumour growth inhibition
-        pharmacodynamic model introduced by Koch et al. in [1].
+        Returns the absolute path to a SBML file, specifying the tumour growth
+        inhibition pharmacodynamic model introduced by Koch et al. in [1]_.
 
-        References:
-        -----------
-        [1] Koch, G. et al. Modeling of tumor growth and anticancer effects of
-        combination therapy. J Pharmacokinet Pharmacodyn 36, 179–197 (2009).
+        In this model the tumour growth inhibition is modelled by an empirical
+        model of the tumour volume :math:`V_T` over time
+
+        .. math::
+            \frac{\text{d}V_T}{\text{d}t} =
+                \frac{2\lambda_0\lambda_1 V_T}
+                {2\lambda_0V_T + \lambda_1} - \kappa C V_T.
+
+
+        Here, the tumour growth in absence of the drug is assumed to grow
+        exponentially at rate :math:`2\lambda_0` for tumour volumes below some
+        critical volume :math:`V_{\text{crit}}`. For volumes beyond
+        :math:`V_{\text{crit}}` the growth dynamics is assumed to slow down
+        and transition to a linear growth at rate :math:`\lambda_0`. The tumour
+        growth inhibitory effect of the compound is modelled proportionally to
+        its concentration :math:`C` and the current tumour volume. The
+        proportionality factor :math:`\kappa` can be interpreted as the potency
+        of the drug.
+
+        Note that the critical tumour volume :math:`V_{\text{crit}}` at which
+        the growth dynamics transtions from exponential to linear growth is
+        given by the two growth rates
+
+        .. math::
+            V_{\text{crit}} = \frac{\lambda _1}{2\lambda _0}.
+
+        .. [1] Koch, G. et al. Modeling of tumor growth and anticancer effects
+               of combination therapy. J Pharmacokinet Pharmacodyn 36, 179–197
+               (2009).
         """
         file_name = 'TGI_Koch_2009.xml'
 
