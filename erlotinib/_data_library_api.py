aba629058767fb65be6495641d661f1291106d22
DavAug <david.augustin@gmx.net>
2020-11-12 15:37:46 +0000

    add standardise flag to methods

--- erlotinib/_data_library_api.py
+++ erlotinib/_data_library_api.py
@@ -23,7 +23,7 @@ class DataLibrary(object):
         self._path = os.path.dirname(os.path.abspath(__file__))
         self._path += '/data_library/'
 
-    def lung_cancer_control_group(self):
+    def lung_cancer_control_group(self, standardised=False):
         r"""
         Returns the lung cancer control group data published in [1]_ as a
         :class:`pandas.DataFrame`.
@@ -33,11 +33,29 @@ class DataLibrary(object):
         mouse was monitored over a period of 30 days and measured a couple
         times a week.
 
+        The original column keys are '#ID', 'TIME in day' and 'TUMOUR VOLUME in
+        cm^3'. If ``standardised=True`` those column keys are changed to the
+        generic keys 'ID', 'Time', and 'Biomarker', which are also used by
+        other classes.
+
         .. [1] Eigenmann, M. J. et al., Combining Nonclinical Experiments with
                Translational PKPD Modeling to Differentiate Erlotinib and
                Gefitinib, Mol Cancer Ther. 2016; 15(12):3110-3119.
+
+        Parameters
+        ----------
+        standardised
+            A boolean flag indicating whether the columns are supposed to be
+            standardised.
+
         """
         file_name = 'lxf_control_growth.csv'
         data = pd.read_csv(self._path + file_name)
+
+        if standardised:
+            data = data.rename(columns={
+                '#ID': 'ID',
+                'TIME in day': 'Time',
+                'TUMOUR VOLUME in cm^3': 'Biomarker'})
 
         return data
