0b70827d77b561f50a5b614ff2ed1000e0c16cf4
DavAug <david.augustin@gmx.net>
2020-11-13 16:06:13 +0000

    add a add_simulation method

--- erlotinib/plots/_time_series.py
+++ erlotinib/plots/_time_series.py
@@ -29,7 +29,7 @@ class PDTimeSeriesPlot(eplt.SingleFigure):
     def __init__(self, updatemenu=True):
         super(PDTimeSeriesPlot, self).__init__(updatemenu)
 
-    def _add_trace(self, label, times, biomarker, color):
+    def _add_data_trace(self, label, times, biomarker, color):
         """
         Adds scatter plot of an indiviudals pharamcodynamics to figure.
         """
@@ -44,6 +44,18 @@ class PDTimeSeriesPlot(eplt.SingleFigure):
                     color=color,
                     opacity=0.7,
                     line=dict(color='black', width=1))))
+
+    def _add_simulation_trace(self, times, biomarker):
+        """
+        Adds scatter plot of an indiviudals pharamcodynamics to figure.
+        """
+        self._fig.add_trace(go.Scatter(
+                x=times,
+                y=biomarker,
+                name="Model",
+                showlegend=True,
+                mode="lines",
+                line=dict(color='black')))
 
     def add_data(
             self, data, id_key='ID', time_key='Time', biom_key='Biomarker'):
@@ -95,4 +107,39 @@ class PDTimeSeriesPlot(eplt.SingleFigure):
             color = colors[index]
 
             # Create Scatter plot
-            self._add_trace(label, times, biomarker, color)
+            self._add_data_trace(label, times, biomarker, color)
+
+    def add_simulation(self, data, time_key='Time', biom_key='Biomarker'):
+        """
+        Adds a pharmacodynamic time series simulation to the figure.
+
+        Expects a :class:`pandas.DataFrame` with a time and a PD biomarker
+        column, and adds a line plot of the biomarker time series to the
+        figure.
+
+        Parameters
+        ----------
+        data
+            A :class:`pandas.DataFrame` with the time series PD simulation in
+            form of a time and biomarker column.
+        time_key
+            Key label of the :class:`DataFrame` which specifies the time
+            column. Defaults to ``'Time'``.
+        biom_key
+            Key label of the :class:`DataFrame` which specifies the PD
+            biomarker column. Defaults to ``'Biomarker'``.
+        """
+        # Check input format
+        if not isinstance(data, pd.DataFrame):
+            raise TypeError(
+                'Data has to be pandas.DataFrame.')
+
+        for key in [time_key, biom_key]:
+            if key not in data.keys():
+                raise ValueError(
+                    'Data does not have the key <' + str(key) + '>.')
+
+        times = data[time_key]
+        biomarker = data[biom_key]
+
+        self._add_simulation_trace(times, biomarker)
