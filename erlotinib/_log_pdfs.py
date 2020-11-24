3797f22dcfd364df77d87780fd7fb55e811be890
DavAug <david.augustin@gmx.net>
2020-11-24 18:20:17 +0000

    Create a ReducedLogPDF wrapper

---
+++ erlotinib/_log_pdfs.py
@@ -0,0 +1,60 @@
+#
+# This file is part of the erlotinib repository
+# (https://github.com/DavAug/erlotinib/) which is released under the
+# BSD 3-clause license. See accompanying LICENSE.md for copyright notice and
+# full license details.
+#
+
+import numpy as np
+import pints
+
+
+class ReducedLogPDF(pints.LogPDF):
+    """
+    A wrapper for a `pints.LogPDF` to fix the values of some model
+    parameters.
+
+    This allows to reduce the parameter dimensionality of the log-pdf
+    at the cost of fixing some parameters at a constant value.
+    """
+
+    def __init__(self, log_pdf, mask, values):
+        super(ReducedLogPDF, self).__init__()
+
+        self._log_pdf = log_pdf
+
+        if len(mask) != self._log_pdf.n_parameters():
+            raise ValueError(
+                'Length of mask has to match the number of log-pdf '
+                'parameters.')
+
+        mask = np.asarray(mask)
+        if mask.dtype != bool:
+            raise ValueError(
+                'Mask has to be a boolean array.')
+
+        n_fixed = int(np.sum(mask))
+        if n_fixed != len(values):
+            raise ValueError(
+                'The have to be as many value inputs as the number of '
+                'fixed parameters.')
+
+        # Create a parameter array for later calls of the log-pdf
+        self._parameters = np.empty(shape=len(mask))
+        self._parameters[mask] = np.asarray(values)
+
+        # Allow for updating the 'free' number of parameters
+        self._mask = ~mask
+        self._n_parameters = int(np.sum(self._mask))
+
+    def __call__(self, parameters):
+        # Fill in 'free' parameters
+        self._parameters[self._mask] = np.asarray(parameters)
+
+        return self._log_pdf(self._parameters)
+
+    def n_parameters(self):
+        """
+        Returns the number of 'free' parameters of the log-posterior.
+        """
+        return self._n_parameters
