0ea092234b78829e61a64aa4bef282acfb41edf3
DavAug <david.augustin@gmx.net>
2020-11-28 10:59:31 +0000

    Create a log-posterior class

--- erlotinib/_log_pdfs.py
+++ erlotinib/_log_pdfs.py
@@ -9,6 +9,76 @@ import numpy as np
 import pints
 
 
+class LogPosterior(pints.LogPosterior):
+    """
+    A log-posterior class which can be used to find maximum a posteriori
+    estimates of model estimates, or sample from the posterior itself.
+
+    Extends :class:`pints.LogPosterior`.
+    """
+
+    def __init__(self, log_likelihood, log_prior):
+        super(LogPosterior, self).__init__(log_likelihood, log_prior)
+
+        # Set defaults
+        self._id = None
+        n_params = self._n_parameters
+        self._parameter_names = ['Param %d' % (n+1) for n in range(n_params)]
+
+    def get_id(self):
+        """
+        Returns the id of the log-posterior. If no id is set, ``None`` is
+        returned.
+        """
+        return self._id
+
+    def get_parameter_names(self):
+        """
+        Returns the names of the model parameters. By default the parameters
+        are enumerated and assigned with the name 'Param #'.
+        """
+        return self._parameter_names
+
+    def set_id(self, posterior_id):
+        """
+        Sets the posterior id.
+
+        This can be used to tag the log-posterior to distinguish it from
+        other structurally identical log-posteriors, e.g. when the same
+        model is used to describe the PKPD of different individuals.
+
+        Parameters
+        ----------
+
+        posterior_id
+            An ID that can be used to identify the log-posterior. A valid ID
+            has to be convertable to a string object.
+        """
+        self._id = str(posterior_id)
+
+    def set_parameter_names(self, names):
+        """
+        Sets the names of the model parameters.
+
+        The list of parameters has to match the length of the number of
+        parameters. The first parameter name in the list is assigned to the
+        first parameter, the second name in the list is assigned to second
+        model parameter, and so on.
+
+        Parameters
+        ----------
+
+        names
+            A list of string-convertable objects that is used to assign names
+            to the model parameters.
+        """
+        if len(names) != self._n_parameters:
+            ValueError(
+                'The list of parameter names has to match the number of model '
+                'parameters.')
+        self._parameter_names = [str(name) for name in names]
+
+
 class ReducedLogPDF(pints.LogPDF):
     """
     A wrapper for a :class:`pints.LogPDF` to fix the values of a subset of
@@ -16,6 +86,8 @@ class ReducedLogPDF(pints.LogPDF):
 
     This allows to reduce the parameter dimensionality of the log-pdf
     at the cost of fixing some parameters at a constant value.
+
+    Extends :class:`pints.LogPDF`.
 
     Parameters
     ----------
