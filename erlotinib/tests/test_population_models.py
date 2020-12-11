d1681cc87cb11d3d435ba91438ccf2cbbc198960
DavAug <david.augustin@gmx.net>
2020-12-11 14:44:18 +0100

    #29 create file to test Populati0nModel

---
+++ erlotinib/tests/test_population_models.py
@@ -0,0 +1,25 @@
+#
+# This file is part of the erlotinib repository
+# (https://github.com/DavAug/erlotinib/) which is released under the
+# BSD 3-clause license. See accompanying LICENSE.md for copyright notice and
+# full license details.
+#
+
+import unittest
+
+import erlotinib as erlo
+
+
+class TestPooledModel(unittest.TestCase):
+    """
+    Tests the erlotinib.PooledModel class.
+    """
+
+class TestPopulationModel(unittest.TestCase):
+    """
+    Tests the erlotinib.PopulationModel class.
+    """
+
+
+if __name__ == '__main__':
+    unittest.main()
