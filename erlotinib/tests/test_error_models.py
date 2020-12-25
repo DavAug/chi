2c8725378155c67bb956dfd73b423c9a20027756
DavAug <david.augustin@gmx.net>
2020-12-25 16:48:22 +0100

    #110 test get_parameter_names

--- erlotinib/tests/test_error_models.py
+++ erlotinib/tests/test_error_models.py
@@ -10,6 +10,40 @@ import unittest
 import erlotinib as erlo
 
 
+class TestConstantAndMultiplicativeGaussianErrorModel(unittest.TestCase):
+    """
+    Tests the erlo.ConstantAndMultiplicativeGaussianErrorModel class.
+    """
+    @classmethod
+    def setUpClass(cls):
+        cls.error_model = erlo.ConstantAndMultiplicativeGaussianErrorModel()
+
+    def test_compute_log_likelihood(self):
+        # TODO:
+        pass
+
+    def test_get_parameter_names(self):
+        parameters = self.error_model.get_parameter_names()
+
+        self.assertEqual(len(parameters), 2)
+        self.assertEqual(parameters[0], 'Sigma base')
+        self.assertEqual(parameters[1], 'Sigma rel.')
+
+    # def test_n_parameters(self):
+    #     self.assertIsNone(self.error_model.n_parameters())
+
+    # def test_sample(self):
+    #     parameters = 'some parameters'
+    #     model_output = 'some output'
+    #     with self.assertRaisesRegex(NotImplementedError, ''):
+    #         self.error_model.sample(parameters, model_output)
+
+    # def test_set_parameter_names(self):
+    #     names = 'some names'
+    #     with self.assertRaisesRegex(NotImplementedError, ''):
+    #         self.error_model.set_parameter_names(names)
+
+
 class TestErrorModel(unittest.TestCase):
     """
     Tests the erlo.ErrorModel class.
