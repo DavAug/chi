04cffed5f386d08df6fabcc54283cffddaed9016
DavAug <david.augustin@gmx.net>
2020-12-26 13:30:30 +0100

    #106 implement sample method

--- erlotinib/_predictive_models.py
+++ erlotinib/_predictive_models.py
@@ -5,6 +5,9 @@
 # full license details.
 #
 
+import numpy as np
+import pandas as pd
+
 import erlotinib as erlo
 
 
@@ -80,3 +83,95 @@ class PredictiveModel(object):
         Returns the number of parameters of the predictive model.
         """
         return self._n_parameters
+
+    def sample(
+            self, parameters, times, n_samples=None, seed=None,
+            return_df=True):
+        """
+        Samples "measurements" of the biomarkers from the predictive model and
+        returns in form of a :class:`pandas.DataFrame`.
+
+        The mechanistic model is solved for the provided parameters and times,
+        and samples around this solution are drawn for time point according to
+        the error models.
+
+        The number of samples for each time point can be specified with
+        ``n_samples``.
+
+        Parameters
+        ----------
+        parameters
+            An array-like object with the parameter values of the predictive
+            model.
+        times
+            An array-like object with times at which the virtual "measurements"
+            are performed.
+        n_samples
+            The number of virtual "measurements" that are performed at each
+            time point. If ``None`` only the biomarkers are measured only once
+            at each time point.
+        seed
+            A seed for the pseudo-random number generator.
+        return_df
+            A boolean flag which determines whether the output is returned as a
+            :class:`pandas.DataFrame` or a :class:`numpy.ndarray`. If ``False``
+            the samples are returned as a numpy array of shape
+            ``(n_outputs, n_times, n_samples)``.
+        """
+        parameters = np.asarray(parameters)
+        if len(parameters) != self._n_parameters:
+            raise ValueError(
+                'The length of parameters does not match n_parameters.')
+
+        # Sort times
+        times = np.sort(times)
+
+        # Sort parameters into mechanistic model params and error params
+        n_parameters = self._mechanistic_model.n_parameters()
+        mechanistic_params = parameters[:n_parameters]
+        error_params = parameters[n_parameters:]
+
+        # Solve mechanistic model
+        outputs = self._mechanistic_model.simulate(mechanistic_params)
+
+        # Create numpy container for samples
+        n_outputs = len(outputs)
+        n_times = len(times)
+        n_samples = n_samples if n_samples is not None else 1
+        container = np.empty(shape=(n_outputs, n_times, n_samples))
+
+        # Sample error around mechanistic model outputs
+        start_index = 0
+        for output_id, error_model in enumerate(self._error_models):
+            end_index = start_index + error_model.n_parameters()
+
+            # Sample
+            container[output_id, ...] = error_model.sample(
+                parameters=error_params[start_index:end_index],
+                model_output=outputs[output_id],
+                n_samples=n_samples,
+                seed=seed)
+
+            # Update start index
+            start_index = end_index
+
+        if return_df is False:
+            # Return samples in numpy array format
+            return container
+
+        # Structure samples in a pandas.DataFrame
+        output_names = self._mechanistic_model.outputs()
+        sample_ids = np.arange(start=1, stop=n_samples+1)
+        samples = pd.DataFrame(
+            columns=['Sample ID', 'Biomarker', 'Time', 'Sample'])
+
+        # Fill in all samples at a specific time point at once
+        for output_id, name in enumerate(output_names):
+            for time_id, time in enumerate(times):
+                samples = samples.append(pd.DataFrame({
+                    'Sample ID': sample_ids,
+                    'Time': [time],
+                    'Biomarker': [name],
+                    'Sample': container[output_id, time_id, :]}))
+
+        return samples
