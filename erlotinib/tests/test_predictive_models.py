066714acb8fdba384fb301753316cd8230934107
DavAug <david.augustin@gmx.net>
2020-12-26 14:07:17 +0100

    #106 test bad instantiation

--- erlotinib/tests/test_predictive_models.py
+++ erlotinib/tests/test_predictive_models.py
@@ -19,13 +19,40 @@ class TestPredictiveModel(unittest.TestCase):
     def setUpClass(cls):
         # Get mechanistic model
         path = erlo.ModelLibrary().tumour_growth_inhibition_model_koch()
-        model = erlo.PharmacodynamicModel(path)
+        cls.mechanistic_model = erlo.PharmacodynamicModel(path)
 
         # Define error models
-        error_models = [erlo.ConstantAndMultiplicativeGaussianErrorModel()]
+        cls.error_models = [erlo.ConstantAndMultiplicativeGaussianErrorModel()]
 
         # Create predictive model
-        cls.model = erlo.PredictiveModel(model, error_models)
+        cls.model = erlo.PredictiveModel(
+            cls.mechanistic_model, cls.error_models)
+
+    def test_bad_instantiation(self):
+        # Mechanistic model has wrong type
+        mechanistic_model = 'wrong type'
+
+        with self.assertRaisesRegex(ValueError, 'The provided mechanistic'):
+            erlo.PredictiveModel(mechanistic_model, self.error_models)
+
+        # Error model has wrong type
+        error_models = ['wrong type']
+
+        with self.assertRaisesRegex(ValueError, 'All provided error models'):
+            erlo.PredictiveModel(self.mechanistic_model, error_models)
+
+        # Non-existent outputs
+        outputs = ['Not', 'existent']
+
+        with self.assertRaisesRegex(KeyError, 'The variable <Not> does not'):
+            erlo.PredictiveModel(
+                self.mechanistic_model, self.error_models, outputs)
+
+        # Wrong number of error models
+        error_models = [erlo.ErrorModel(), erlo.ErrorModel()]
+
+        with self.assertRaisesRegex(ValueError, 'Wrong number of error'):
+            erlo.PredictiveModel(self.mechanistic_model, error_models)
 
     def test_get_parameter_names(self):
         names = self.model.get_parameter_names()
