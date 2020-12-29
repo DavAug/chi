e2b4bec127bb93d0385a0cfa6d83f05b8e2c17a0
DavAug <david.augustin@gmx.net>
2020-12-29 13:15:16 +0100

    #120 test sample

--- erlotinib/tests/test_predictive_models.py
+++ erlotinib/tests/test_predictive_models.py
@@ -8,6 +8,7 @@
 import unittest
 
 import pandas as pd
+import pints
 
 import erlotinib as erlo
 
@@ -220,6 +221,153 @@ class TestPredictiveModel(unittest.TestCase):
 
         with self.assertRaisesRegex(ValueError, 'The length of parameters'):
             self.model.sample(parameters, times)
+
+
+class TestPriorPredictiveModel(unittest.TestCase):
+    """
+    Tests the erlo.PriorPredictiveModel class.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        # Get mechanistic model
+        path = erlo.ModelLibrary().tumour_growth_inhibition_model_koch()
+        mechanistic_model = erlo.PharmacodynamicModel(path)
+
+        # Define error models
+        error_models = [erlo.ConstantAndMultiplicativeGaussianErrorModel()]
+
+        # Create predictive model
+        cls.predictive_model = erlo.PredictiveModel(
+            mechanistic_model, error_models)
+
+        # Create prior
+        cls.log_prior = pints.ComposedLogPrior(
+            pints.UniformLogPrior(0, 1),
+            pints.UniformLogPrior(1, 2),
+            pints.UniformLogPrior(2, 3),
+            pints.UniformLogPrior(3, 4),
+            pints.UniformLogPrior(4, 5),
+            pints.UniformLogPrior(5, 6),
+            pints.UniformLogPrior(6, 7))
+
+        # Create prior predictive model
+        cls.model = erlo.PriorPredictiveModel(
+            cls.predictive_model, cls.log_prior)
+
+    def test_bad_instantiation(self):
+        # Predictive model has wrong type
+        predictive_model = 'wrong type'
+
+        with self.assertRaisesRegex(ValueError, 'The provided predictive'):
+            erlo.PriorPredictiveModel(predictive_model, self.log_prior)
+
+        # Prior has woring type
+        log_prior = 'wrong type'
+
+        with self.assertRaisesRegex(ValueError, 'The provided log-prior'):
+            erlo.PriorPredictiveModel(self.predictive_model, log_prior)
+
+        # Dimension of predictive model and log-prior don't match
+        log_prior = pints.UniformLogPrior(0, 1)  # dim 1, but 7 params
+
+        with self.assertRaisesRegex(ValueError, 'The dimension of the'):
+            erlo.PriorPredictiveModel(self.predictive_model, log_prior)
+
+    def test_sample(self):
+        # Test case I: Just one sample
+        times = [1, 2, 3, 4, 5]
+        seed = 42
+        samples = self.model.sample(times, seed=seed)
+
+        self.assertIsInstance(samples, pd.DataFrame)
+
+        keys = samples.keys()
+        self.assertEqual(len(keys), 4)
+        self.assertEqual(keys[0], 'Sample ID')
+        self.assertEqual(keys[1], 'Biomarker')
+        self.assertEqual(keys[2], 'Time')
+        self.assertEqual(keys[3], 'Sample')
+
+        sample_ids = samples['Sample ID'].unique()
+        self.assertEqual(len(sample_ids), 1)
+        self.assertEqual(sample_ids[0], 1)
+
+        biomarkers = samples['Biomarker'].unique()
+        self.assertEqual(len(biomarkers), 1)
+        self.assertEqual(biomarkers[0], 'myokit.tumour_volume')
+
+        times = samples['Time'].unique()
+        self.assertEqual(len(times), 5)
+        self.assertEqual(times[0], 1)
+        self.assertEqual(times[1], 2)
+        self.assertEqual(times[2], 3)
+        self.assertEqual(times[3], 4)
+        self.assertEqual(times[4], 5)
+
+        values = samples['Sample'].unique()
+        self.assertEqual(len(values), 5)
+        self.assertAlmostEqual(values[0], 2.8622881485041396)
+        self.assertAlmostEqual(values[1], 3.7272644272099664)
+        self.assertAlmostEqual(values[2], -2.5604320890107455)
+        self.assertAlmostEqual(values[3], -5.445074975020219)
+        self.assertAlmostEqual(values[4], -8.562594546870663)
+
+        # Test case II: More than one sample
+        n_samples = 4
+        samples = self.model.sample(
+            times, n_samples=n_samples, seed=seed)
+
+        self.assertIsInstance(samples, pd.DataFrame)
+
+        keys = samples.keys()
+        self.assertEqual(len(keys), 4)
+        self.assertEqual(keys[0], 'Sample ID')
+        self.assertEqual(keys[1], 'Biomarker')
+        self.assertEqual(keys[2], 'Time')
+        self.assertEqual(keys[3], 'Sample')
+
+        sample_ids = samples['Sample ID'].unique()
+        self.assertEqual(len(sample_ids), 4)
+        self.assertEqual(sample_ids[0], 1)
+        self.assertEqual(sample_ids[1], 2)
+        self.assertEqual(sample_ids[2], 3)
+        self.assertEqual(sample_ids[3], 4)
+
+        biomarkers = samples['Biomarker'].unique()
+        self.assertEqual(len(biomarkers), 1)
+        self.assertEqual(biomarkers[0], 'myokit.tumour_volume')
+
+        times = samples['Time'].unique()
+        self.assertEqual(len(times), 5)
+        self.assertEqual(times[0], 1)
+        self.assertEqual(times[1], 2)
+        self.assertEqual(times[2], 3)
+        self.assertEqual(times[3], 4)
+        self.assertEqual(times[4], 5)
+
+        values = samples['Sample'].unique()
+        self.assertEqual(len(values), 20)
+        self.assertAlmostEqual(values[0], 3.6599791844429284)
+        self.assertAlmostEqual(values[1], -12.696938587267084)
+        self.assertAlmostEqual(values[2], -3.82460662961628)
+        self.assertAlmostEqual(values[3], -4.103207219325659)
+        self.assertAlmostEqual(values[4], -5.196420346964001)
+        self.assertAlmostEqual(values[5], 10.726522931974097)
+        self.assertAlmostEqual(values[6], 1.4866633054676286)
+        self.assertAlmostEqual(values[7], 5.48736409468915)
+        self.assertAlmostEqual(values[8], -4.211329523375031)
+        self.assertAlmostEqual(values[9], -2.38819374047191)
+        self.assertAlmostEqual(values[10], -3.6298125294796812)
+        self.assertAlmostEqual(values[11], 9.209895487514647)
+        self.assertAlmostEqual(values[12], -6.256268368313989)
+        self.assertAlmostEqual(values[13], -5.03014957524413)
+        self.assertAlmostEqual(values[14], 6.367870976692225)
+        self.assertAlmostEqual(values[15], -1.2252254747096893)
+        self.assertAlmostEqual(values[16], -0.7853509638638059)
+        self.assertAlmostEqual(values[17], 12.177527343575)
+        self.assertAlmostEqual(values[18], -6.435165240274607)
+        self.assertAlmostEqual(values[19], 10.471501140030037)
 
 
 if __name__ == '__main__':
