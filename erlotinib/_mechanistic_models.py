f1797678c0f11a1f7109dd4a091f09e3c7e1f0d3
DavAug <david.augustin@gmx.net>
2021-01-05 16:40:16 +0100

    #111 RMM add method n_fixed_parameters

--- erlotinib/_mechanistic_models.py
+++ erlotinib/_mechanistic_models.py
@@ -728,6 +728,17 @@ class ReducedMechanisticModel(object):
         """
         return self._mechanistic_model
 
+    def n_fixed_parameters(self):
+        """
+        Returns the number of fixed model parameters.
+        """
+        if self._fixed_params_mask is None:
+            return 0
+
+        n_fixed = int(np.sum(self._fixed_params_mask))
+
+        return n_fixed
+
     def n_outputs(self):
         """
         Returns the number of output dimensions.
