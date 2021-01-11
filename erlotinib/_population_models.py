38df9d3ebc344b442e3fb1d2999c5a46d9c24474
DavAug <david.augustin@gmx.net>
2021-01-11 11:56:53 +0100

    #107 implementation of some RPM methods

--- erlotinib/_population_models.py
+++ erlotinib/_population_models.py
@@ -595,3 +595,145 @@ class PooledModel(PopulationModel):
                 'Length of names does not match n_top_parameters.')
 
         self._top_parameter_names = [str(label) for label in names]
+
+
+class ReducedPopulationModel(object):
+    """
+    #TODO:
+    # 1. Implement
+    # 2. Test RPM
+    # 3. Try PredPopModel in notebook
+    # 4. Write tests for PPM
+    A base class for population models.
+
+    Parameters
+    ----------
+    n_ids
+        Number of individual bottom level models. Defaults to 1.
+    """
+
+    def __init__(self, population_model):
+        super(ReducedPopulationModel, self).__init__()
+
+        # Check inputs
+        if not isinstance(population_model, PopulationModel):
+            raise TypeError(
+                'The population model has to be an instance of a '
+                'erlotinib.PopulationModel.')
+
+        self._population_model = population_model
+
+    def __call__(self, parameters):
+        """
+        Returns the log-likelihood score of the population model.
+
+        The parameters are expected to be of length :meth:`n_parameters`. The
+        first :meth:`n_bottom_parameters` parameters are treated as the
+        'observations' of the individual model parameters, and the remaining
+        :meth:`n_top_parameters` specify the values of the population
+        model parameters.
+        """
+        return self._population_model(parameters)
+
+    def get_bottom_parameter_name(self):
+        """
+        Returns the name of the the modelled bottom parameter. If name was not
+        set, ``None`` is returned.
+        """
+        return self._population_model.get_bottom_parameter_name()
+
+    def get_ids(self):
+        """
+        Returns the IDs of the modelled individuals. If IDs were not set,
+        ``None`` is returned.
+        """
+        return self._population_model.get_ids()
+
+    def get_top_parameter_names(self):
+        """
+        Returns the name of the the population model parameters. If name were
+        not set, defaults are returned.
+        """
+        return self._population_model.get_top_parameter_names()
+
+    def n_bottom_parameters(self):
+        """
+        Returns the number of bottom-level parameters of the population model.
+
+        This is the total number of input parameters from the individual
+        likelihoods.
+        """
+        return self._population_model.get_bottom_parameter_names()
+
+    def n_ids(self):
+        """
+        Returns the number of modelled individuals of the population model.
+        """
+        return self._population_model.n_ids()
+
+    def n_parameters(self):
+        """
+        Returns the number of parameters of the population model.
+        """
+        return self._population_model.n_parameters()
+
+    def n_parameters_per_id(self):
+        """
+        Returns the number of parameters per likelihood that are modelled by
+        the population model.
+        """
+        return self._population_model.n_parameters_per_id()
+
+    def n_top_parameters(self):
+        """
+        Returns the number of top parameters of the population.
+
+        This is the number of population parameters.
+        """
+        return self._population_model.n_top_parameters()
+
+    def sample(self, top_parameters, n_samples=None, seed=None):
+        r"""
+        Returns random samples from the underlying population distribution.
+
+        The returned value is a NumPy array with shape ``(n_samples,)``.
+
+        Parameters
+        ----------
+        top_parameters
+            Parameter values of the top-level parameters that are used for the
+            simulation.
+        n_samples
+            Number of samples. If ``None``, one sample is returned.
+        seed
+            A seed for the pseudo-random number generator.
+        """
+        #TODO:
+        return self._population_model.sample(top_parameters, n_samples, seed)
+
+    def set_bottom_parameter_name(self, name):
+        """
+        Sets the name of the input parameter from each individual bottom model.
+        """
+        self._bottom_parameter_name = str(name)
+
+    def set_ids(self, ids):
+        """
+        Sets the IDs of the modelled individuals.
+
+        Parameters
+        ----------
+        ids
+            A list of ids of length ``n_ids``.
+        """
+        if len(ids) != self._n_ids:
+            raise ValueError(
+                'Length of IDs does not match n_ids.')
+
+        self._ids = [str(label) for label in ids]
+
+    def set_top_parameter_names(self, names):
+        """
+        Sets the names of the population model parameters.
+        """
+        raise NotImplementedError
