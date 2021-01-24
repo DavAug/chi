8dd6f0d9152e8b912d63162026f6531b5fd83465
DavAug <david.augustin@gmx.net>
2021-01-24 14:45:12 +0100

    #131 test set_population_model bad input

--- erlotinib/tests/test_problems.py
+++ erlotinib/tests/test_problems.py
@@ -1065,125 +1065,111 @@ class TestProblemModellingControllerPDProblem(unittest.TestCase):
         self.assertEqual(
             param_names[32], 'Std. myokit.tumour_volume Sigma rel.')
 
-    # def test_set_population_model_bad_input(self):
-    #     # No mechanistic model set
-    #     problem = erlo.ProblemModellingController(
-    #         self.data, biom_keys=['Biomarker'])
+    def test_set_population_model_bad_input(self):
+        # Population models have the wrong type
+        pop_models = ['bad', 'type']
+        with self.assertRaisesRegex(TypeError, 'The population models'):
+            self.pd_problem.set_population_model(pop_models)
 
-    #     with self.assertRaisesRegex(ValueError, 'Before setting'):
-    #         problem.set_population_model(self.pop_models)
+        # Number of population models is not correct
+        pop_models = [erlo.PooledModel()]
+        with self.assertRaisesRegex(ValueError, 'The number of population'):
+            self.pd_problem.set_population_model(pop_models)
 
-    #     # No error model set
-    #     problem.set_mechanistic_model(self.model)
-
-    #     with self.assertRaisesRegex(ValueError, 'Before setting'):
-    #         problem.set_population_model(self.pop_models)
-
-    #     # Population models have the wrong type
-    #     problem.set_error_model(self.error_models)
-
-    #     pop_models = ['bad', 'type']
-    #     with self.assertRaisesRegex(ValueError, 'The population models'):
-    #         problem.set_population_model(pop_models)
-
-    #     # The specified parameters do not match the model parameters
-    #     pop_models = [erlo.PooledModel(), erlo.HeterogeneousModel()]
-    #     params = ['wrong', 'outputs']
-    #     with self.assertRaisesRegex(ValueError, 'The parameter <wrong>'):
-    #         problem.set_population_model(pop_models, params)
-
-    #     # Not one population model for each parameter
-    #     with self.assertRaisesRegex(ValueError, 'If no parameter names are'):
-    #         problem.set_population_model(pop_models)
+        # Specified parameter names do not coincide with model
+        pop_models = [erlo.PooledModel()] * 7
+        parameter_names = ['wrong names'] * 7
+        with self.assertRaisesRegex(ValueError, 'The parameter names'):
+            self.pd_problem.set_population_model(pop_models, parameter_names)
 
 
-# class TestInverseProblem(unittest.TestCase):
-#     """
-#     Tests the erlotinib.InverseProblem class.
-#     """
+class TestInverseProblem(unittest.TestCase):
+    """
+    Tests the erlotinib.InverseProblem class.
+    """
 
-#     @classmethod
-#     def setUpClass(cls):
-#         # Create test data
-#         cls.times = [1, 2, 3, 4, 5]
-#         cls.values = [1, 2, 3, 4, 5]
+    @classmethod
+    def setUpClass(cls):
+        # Create test data
+        cls.times = [1, 2, 3, 4, 5]
+        cls.values = [1, 2, 3, 4, 5]
 
-#         # Set up inverse problem
-#         path = erlo.ModelLibrary().tumour_growth_inhibition_model_koch()
-#         cls.model = erlo.PharmacodynamicModel(path)
-#         cls.problem = erlo.InverseProblem(cls.model, cls.times, cls.values)
+        # Set up inverse problem
+        path = erlo.ModelLibrary().tumour_growth_inhibition_model_koch()
+        cls.model = erlo.PharmacodynamicModel(path)
+        cls.problem = erlo.InverseProblem(cls.model, cls.times, cls.values)
 
-#     def test_bad_model_input(self):
-#         model = 'bad model'
+    def test_bad_model_input(self):
+        model = 'bad model'
 
-#         with self.assertRaisesRegex(ValueError, 'Model has to be an instance'):
-#             erlo.InverseProblem(model, self.times, self.values)
+        with self.assertRaisesRegex(ValueError, 'Model has to be an instance'):
+            erlo.InverseProblem(model, self.times, self.values)
 
-#     def test_bad_times_input(self):
-#         times = [-1, 2, 3, 4, 5]
-#         with self.assertRaisesRegex(ValueError, 'Times cannot be negative.'):
-#             erlo.InverseProblem(self.model, times, self.values)
+    def test_bad_times_input(self):
+        times = [-1, 2, 3, 4, 5]
+        with self.assertRaisesRegex(ValueError, 'Times cannot be negative.'):
+            erlo.InverseProblem(self.model, times, self.values)
 
-#         times = [5, 4, 3, 2, 1]
-#         with self.assertRaisesRegex(ValueError, 'Times must be increasing.'):
-#             erlo.InverseProblem(self.model, times, self.values)
+        times = [5, 4, 3, 2, 1]
+        with self.assertRaisesRegex(ValueError, 'Times must be increasing.'):
+            erlo.InverseProblem(self.model, times, self.values)
 
-#     def test_bad_values_input(self):
-#         values = [1, 2, 3, 4, 5, 6, 7]
-#         with self.assertRaisesRegex(ValueError, 'Values array must have'):
-#             erlo.InverseProblem(self.model, self.times, values)
+    def test_bad_values_input(self):
+        values = [1, 2, 3, 4, 5, 6, 7]
+        with self.assertRaisesRegex(ValueError, 'Values array must have'):
+            erlo.InverseProblem(self.model, self.times, values)
 
-#         values = [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5]]
-#         with self.assertRaisesRegex(ValueError, 'Values array must have'):
-#             erlo.InverseProblem(self.model, self.times, values)
+        values = [[1, 2, 3, 4, 5], [1, 2, 3, 4, 5]]
+        with self.assertRaisesRegex(ValueError, 'Values array must have'):
+            erlo.InverseProblem(self.model, self.times, values)
 
-#     def test_evaluate(self):
-#         parameters = [0.1, 1, 1, 1, 1]
-#         output = self.problem.evaluate(parameters)
+    def test_evaluate(self):
+        parameters = [0.1, 1, 1, 1, 1]
+        output = self.problem.evaluate(parameters)
 
-#         n_times = 5
-#         n_outputs = 1
-#         self.assertEqual(output.shape, (n_times, n_outputs))
+        n_times = 5
+        n_outputs = 1
+        self.assertEqual(output.shape, (n_times, n_outputs))
 
-#     def test_evaluateS1(self):
-#         parameters = [0.1, 1, 1, 1, 1]
-#         with self.assertRaises(NotImplementedError):
-#             self.problem.evaluateS1(parameters)
+    def test_evaluateS1(self):
+        parameters = [0.1, 1, 1, 1, 1]
+        with self.assertRaises(NotImplementedError):
+            self.problem.evaluateS1(parameters)
 
-#     def test_n_ouputs(self):
-#         self.assertEqual(self.problem.n_outputs(), 1)
+    def test_n_ouputs(self):
+        self.assertEqual(self.problem.n_outputs(), 1)
 
-#     def test_n_parameters(self):
-#         self.assertEqual(self.problem.n_parameters(), 5)
+    def test_n_parameters(self):
+        self.assertEqual(self.problem.n_parameters(), 5)
 
-#     def test_n_times(self):
-#         n_times = len(self.times)
-#         self.assertEqual(self.problem.n_times(), n_times)
+    def test_n_times(self):
+        n_times = len(self.times)
+        self.assertEqual(self.problem.n_times(), n_times)
 
-#     def test_times(self):
-#         times = self.problem.times()
-#         n_times = len(times)
+    def test_times(self):
+        times = self.problem.times()
+        n_times = len(times)
 
-#         self.assertEqual(n_times, 5)
+        self.assertEqual(n_times, 5)
 
-#         self.assertEqual(times[0], self.times[0])
-#         self.assertEqual(times[1], self.times[1])
-#         self.assertEqual(times[2], self.times[2])
-#         self.assertEqual(times[3], self.times[3])
-#         self.assertEqual(times[4], self.times[4])
+        self.assertEqual(times[0], self.times[0])
+        self.assertEqual(times[1], self.times[1])
+        self.assertEqual(times[2], self.times[2])
+        self.assertEqual(times[3], self.times[3])
+        self.assertEqual(times[4], self.times[4])
 
-#     def test_values(self):
-#         values = self.problem.values()
+    def test_values(self):
+        values = self.problem.values()
 
-#         n_times = 5
-#         n_outputs = 1
-#         self.assertEqual(values.shape, (n_times, n_outputs))
+        n_times = 5
+        n_outputs = 1
+        self.assertEqual(values.shape, (n_times, n_outputs))
 
-#         self.assertEqual(values[0], self.values[0])
-#         self.assertEqual(values[1], self.values[1])
-#         self.assertEqual(values[2], self.values[2])
-#         self.assertEqual(values[3], self.values[3])
-#         self.assertEqual(values[4], self.values[4])
+        self.assertEqual(values[0], self.values[0])
+        self.assertEqual(values[1], self.values[1])
+        self.assertEqual(values[2], self.values[2])
+        self.assertEqual(values[3], self.values[3])
+        self.assertEqual(values[4], self.values[4])
 
 
 if __name__ == '__main__':
