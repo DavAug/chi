f3cb366a97737902e87bac060bf8e718c1f8c4c8
DavAug <david.augustin@gmx.net>
2021-01-27 19:26:32 +0100

    #153 test webhook pt 1

--- erlotinib/_mechanistic_models.py
+++ erlotinib/_mechanistic_models.py
@@ -206,6 +206,7 @@ class MechanisticModel(object):
         Returns the model's unit of time.
         """
         return self._time_unit
+        
 
 
 class PharmacodynamicModel(MechanisticModel):
