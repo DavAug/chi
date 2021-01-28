a538f3980e8872c4055774fd5ae9b11addde8b4c
DavAug <david.augustin@gmx.net>
2021-01-28 10:54:50 +0100

    #152 test MultplicativeErrorModel

--- erlotinib/tests/test_error_models.py
+++ erlotinib/tests/test_error_models.py
@@ -238,6 +238,173 @@ class TestErrorModel(unittest.TestCase):
             self.error_model.set_parameter_names(names)
 
 
+class TestMultiplicativeGaussianErrorModel(unittest.TestCase):
+    """
+    Tests the erlotinib.MultiplicativeGaussianErrorModel class.
+    """
+
+    @classmethod
+    def setUpClass(cls):
+        cls.error_model = erlo.MultiplicativeGaussianErrorModel()
+
+    def test_compute_log_likelihood(self):
+        # Test case I: If X = X^m, the score reduces to
+        # - np.log(sigma_tot)
+
+        # Test case I.1:
+        parameters = [0.1]
+        model_output = [1] * 10
+        observations = [1] * 10
+        ref_score = -10 * np.log(0.1 * 1)
+
+        score = self.error_model.compute_log_likelihood(
+            parameters, model_output, observations)
+
+        self.assertAlmostEqual(score, ref_score)
+
+        # Test case I.2:
+        parameters = [0.1]
+        model_output = [10] * 10
+        observations = [10] * 10
+        ref_score = -10 * np.log(0.1 * 10)
+
+        score = self.error_model.compute_log_likelihood(
+            parameters, model_output, observations)
+
+        self.assertEqual(score, ref_score)
+
+        # Test case II: If sigma_tot = 1, the score reduces to
+        # -(X-X^m) / 2
+
+        # Test case II.1:
+        parameters = [1]
+        model_output = [1] * 10
+        observations = [2] * 10
+        ref_score = -10 * (1 - 2)**2 / 2
+
+        score = self.error_model.compute_log_likelihood(
+            parameters, model_output, observations)
+
+        self.assertAlmostEqual(score, ref_score)
+
+        # Test case II.2:
+        parameters = [0.1]
+        model_output = [10] * 10
+        observations = [100] * 10
+        ref_score = -10 * (10 - 100)**2 / 2
+
+        score = self.error_model.compute_log_likelihood(
+            parameters, model_output, observations)
+
+        self.assertAlmostEqual(score, ref_score)
+
+        # Test case III: -Infinity for not allowed regimes
+
+        # Test case III.1: Zero sigma_rel
+        parameters = [0]
+        model_output = [1] * 10
+        observations = [1] * 10
+        ref_score = -np.inf
+
+        score = self.error_model.compute_log_likelihood(
+            parameters, model_output, observations)
+
+        self.assertAlmostEqual(score, ref_score)
+
+        # Test case III.2: Negative sigma_rel
+        parameters = [-1]
+        model_output = [1] * 10
+        observations = [1] * 10
+        ref_score = -np.inf
+
+        score = self.error_model.compute_log_likelihood(
+            parameters, model_output, observations)
+
+        self.assertAlmostEqual(score, ref_score)
+
+    def test_compute_log_likelihood_bad_input(self):
+        # Model output and observations don't match
+        parameters = [0.1]
+        model_output = ['some', 'length']
+        observations = ['some', 'other', 'length']
+        with self.assertRaisesRegex(ValueError, 'The number of model outputs'):
+            self.error_model.compute_log_likelihood(
+                parameters, model_output, observations)
+
+    def test_get_parameter_names(self):
+        parameters = self.error_model.get_parameter_names()
+
+        self.assertEqual(len(parameters), 1)
+        self.assertEqual(parameters[0], 'Sigma rel.')
+
+    def test_n_parameters(self):
+        self.assertEqual(self.error_model.n_parameters(), 1)
+
+    def test_sample(self):
+        # Test I: sample size 1
+        seed = 42
+        parameters = [2]
+        n_times = 3
+        model_output = [1, 10, 100]
+        sample = self.error_model.sample(parameters, model_output, seed=seed)
+
+        n_samples = 1
+        self.assertEqual(sample.shape, (n_times, n_samples))
+        self.assertAlmostEqual(sample[0, 0], 1.6094341595088628)
+        self.assertAlmostEqual(sample[1, 0], -10.799682124809912)
+        self.assertAlmostEqual(sample[2, 0], 250.09023916129144)
+
+        # Test II: sample size > 1
+        n_samples = 4
+        sample = self.error_model.sample(
+            parameters, model_output, n_samples=n_samples, seed=seed)
+
+        self.assertEqual(sample.shape, (n_times, n_samples))
+        self.assertAlmostEqual(sample[0, 0], 1.6094341595088628)
+        self.assertAlmostEqual(sample[1, 0], -29.020703773076725)
+        self.assertAlmostEqual(sample[2, 0], 96.63976849914224)
+        self.assertAlmostEqual(sample[0, 1], -1.079968212480991)
+        self.assertAlmostEqual(sample[1, 1], -16.043590137246362)
+        self.assertAlmostEqual(sample[2, 1], -70.608785514716)
+        self.assertAlmostEqual(sample[0, 2], 2.5009023916129145)
+        self.assertAlmostEqual(sample[1, 2], 12.556808063345708)
+        self.assertAlmostEqual(sample[2, 2], 275.8795949725657)
+        self.assertAlmostEqual(sample[0, 3], 2.8811294327824277)
+        self.assertAlmostEqual(sample[1, 3], 3.6751481531283563)
+        self.assertAlmostEqual(sample[2, 3], 255.55838708578966)
+
+    def test_sample_bad_input(self):
+        # Too many paramaters
+        parameters = [1, 1, 1, 1, 1]
+        model_output = [1] * 10
+
+        with self.assertRaisesRegex(ValueError, 'The number of provided'):
+            self.error_model.sample(parameters, model_output)
+
+    def test_set_parameter_names(self):
+        # Set parameter names
+        names = ['some name']
+        self.error_model.set_parameter_names(names)
+        parameters = self.error_model.get_parameter_names()
+
+        self.assertEqual(len(parameters), 1)
+        self.assertEqual(parameters[0], 'some name')
+
+        # Reset parameter names
+        names = None
+        self.error_model.set_parameter_names(names)
+        parameters = self.error_model.get_parameter_names()
+
+        self.assertEqual(len(parameters), 1)
+        self.assertEqual(parameters[0], 'Sigma rel.')
+
+    def test_set_parameter_names_bad_input(self):
+        # Not the right number of names
+        names = ['Too', 'many', 'names']
+        with self.assertRaisesRegex(ValueError, 'Length of names'):
+            self.error_model.set_parameter_names(names)
+
+
 class TestReducedErrorModel(unittest.TestCase):
     """
     Tests the erlotinib.ReducedErrorModel class.
