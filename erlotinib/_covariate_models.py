38f38a97e3dab4fe3ee0724eefeeec511cc202a3
DavAug <david.augustin@gmx.net>
2021-07-28 12:18:58 +0100

    #179 first draft doc string

---
+++ erlotinib/_covariate_models.py
@@ -0,0 +1,49 @@
+#
+# This file is part of the erlotinib repository
+# (https://github.com/DavAug/erlotinib/) which is released under the
+# BSD 3-clause license. See accompanying LICENSE.md for copyright notice and
+# full license details.
+#
+
+
+class CovariateModel(object):
+    r"""
+    A base class for covariate models.
+
+    A CovariateModel assumes that the individual parameters :math:`\psi` are
+    distributed according to a population model that is conditional on the
+    model parameters :math:`\vartheta` and the covariates :math:`\chi`
+
+    .. math::
+        \psi \sim \mathbb{P}(\cdot | \vartheta, \chi).
+
+    Here, covariates can vary from one individual to the next, while the model
+    parameters :math:`\vartheta` are the same for all individuals.
+
+    To simplify this dependence, CovariateModels make the assumption that the
+    distribution :math:`\mathbb{P}(\psi | \vartheta, \chi)` deterministically
+    varies with the covariates, such that the problem can be recast in terms
+    of a covariate-independent distribution of inter-individual
+    fluctuations :math:`\eta`
+
+    .. math::
+        \eta \sim \mathbb{P}(\cdot | \theta)
+
+    and a set of deterministic relationships for the individual parameters
+    :math:`\psi`  and the new population parameters :math:`\theta`
+
+    .. math::
+        \theta = f(\vartheta)
+        \psi = g(\vartheta , \eta, \chi ).
+
+    This base class provides an API to implement the functions :math:`f` and
+    :math:`g`.
+    """
+
+    def __init__(self):
+        super(CovariateModel, self).__init__()
+
+    def compute_population_parameters(self, parameters):
+        r"""
+        Returns the parameters of the population model for :math:`\eta`.
+        """
