762cb91f18c1450bdc805cd8fe2f904ba87c7ad2
DavAug <david.augustin@gmx.net>
2021-07-29 13:12:17 +0100

    #179 flake 8

--- erlotinib/tests/test_population_models.py
+++ erlotinib/tests/test_population_models.py
@@ -2418,7 +2418,8 @@ class TestTruncatedGaussianModel(unittest.TestCase):
         # Negative std
         parameters = [1, -1]
 
-        with self.assertRaisesRegex(ValueError, 'A truncated Gaussian distribution'):
+        with self.assertRaisesRegex(
+                ValueError, 'A truncated Gaussian distribution'):
             self.pop_model.sample(parameters)
 
     def test_set_parameter_names(self):
