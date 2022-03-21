74e3d8d0e6d961e6c3e90609b7c070e2632dbdaf
DavAug <david.augustin@gmx.net>
2022-03-21 19:33:57 +0000

    #255 doc tweaks

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -96,7 +96,7 @@ suppress_warnings = [
 ]
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'emacs'
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
